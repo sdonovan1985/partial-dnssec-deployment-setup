d3c95355231cd1ad514d87c3fcf7bd4a969429ac
Sean Donovan <sdonovan@gatech.edu>
2015-05-22 19:07:20 -0400

    Reworked topology. NAT's not working yet.

--- partial-dnssec-ctlr.py
+++ partial-dnssec-ctlr.py
@@ -5,6 +5,7 @@ from pyretic.lib.query import *
 
 def main():
     return  if_(match(dstip=IPAddr('10.0.0.1')), fwd(1),
-                if_(match(dstip=IPAddr('10.0.0.2')), fwd(2), 
-                    fwd(3)))
+                if_(match(dstip=IPAddr('10.0.0.2')), fwd(2),
+                    if_(match(dstip=IPAddr('10.0.0.3')), fwd(3),
+                        fwd(4))))
 
--- partial-dnssec-topo.py
+++ partial-dnssec-topo.py
@@ -15,20 +15,24 @@ class partial_dnssec_topo(Topo):
         Topology is simple. There are two hosts and one switch.
                Internet
                   |
-              NAT switch
-                |    |
-            +---+    +---+
-            |            |
-        Resolver       Local DNS
+              NAT switch-------------+
+                |    |               |
+            +---+    +---+           |
+            |            |           |
+        Resolver       Local DNS   Host
     
         The NAT switch connects up to the Internet, and both hosts connect via
         the switch. Each host is the same *except* for config files for BIND.
+        The host is just a simple host that's not running bind, but has its DNS
+        server set to Local DNS.
         '''
 
         Topo.__init__(self, **params)
         # Thes two tuples are to use outside director
         replaced_dirs = [ ('/var/log', '/home/mininet/partial-dnssec-deployment-setup/config/%(name)s/var/log'),
                           ('/etc/bind', '/home/mininet/partial-dnssec-deployment-setup/config/%(name)s/etc/bind')]
+        host_dirs = [ ('/var/log', '/home/mininet/partial-dnssec-deployment-setup/config/%(name)s/var/log'),
+                          ('/etc/network', '/home/mininet/partial-dnssec-deployment-setup/config/%(name)s/etc/network')]
         
         linkconfig = {'bw': 10, 'delay': '1ms', 'loss': 0,
                       'max_queue_size': max_queue_size }
@@ -36,15 +40,16 @@ class partial_dnssec_topo(Topo):
         self.nat_switch = self.addSwitch('s1')
         self.resolver = self.addHost('h1', privateDirs=replaced_dirs)
         self.localdns = self.addHost('h2', privateDirs=replaced_dirs)
+        self.simple_host = self.addHost('h3', privateDirs=host_dirs)
 
         self.addLink(self.nat_switch, self.resolver, port1=1, **linkconfig)
         self.addLink(self.nat_switch, self.localdns, port1=2, **linkconfig)
+        self.addLink(self.nat_switch, self.simple_host, port1=3, **linkconfig)
 
 
-    def start_bind(self):
+    def start_configuration(self):
         Node(self.resolver).cmd('service bind9 start')
         Node(self.localdns).cmd('service bind9 start')
-        
         
 if __name__ == '__main__':
     print "Entry"
@@ -53,7 +58,7 @@ if __name__ == '__main__':
     print "created topology"
     rootnode = connectToInternet(net, switch='s1')
     print "connectToInternet returned"
-    net.topo.start_bind()
+    net.topo.start_configuration()
 
     CLI(net)
     stopNAT(rootnode)
